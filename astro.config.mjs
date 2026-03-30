e2952c954065620baea49419c5493168a653d64d
suzu <81377203+JustTuturu@users.noreply.github.com>
2026-03-31 05:08:27 +0700

    Barebone commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+
+import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'static',
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });

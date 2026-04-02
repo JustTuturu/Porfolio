6b07d8516722f46bbdd2fb16d581fef37bc2d13d
suzu <81377203+JustTuturu@users.noreply.github.com>
2026-04-02 18:35:20 +0700

    Final commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  adapter: vercel(),
   vite: {
-    plugins: [tailwindcss()] ,
+    plugins: [tailwindcss()],
     build: {
       sourcemap: false
     },

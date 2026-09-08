ab7b07d171df6971f4bd3176a8feb11dc4d120c6
Tut <81377203+JustTuturu@users.noreply.github.com>
2026-09-08 13:58:10 +0700

    wip: save local change before pull

--- src/data/playlist.ts
+++ src/data/playlist.ts
@@ -14,4 +14,18 @@ export const PLAYLIST: Track[] = [
     cover: 'https://i1.sndcdn.com/artworks-XJKG3kwFvvs3lqBo-aunpzQ-t500x500.jpg',
     src: '/music/lingering-summer-light.mp3',
   },
+  {
+    title: 'Dreams Adrift in Coffee',
+    artist: 'Gray Kamuix',
+    album: 'Wuthering Waves 2.8 OST',
+    cover: 'https://i1.sndcdn.com/artworks-XJKG3kwFvvs3lqBo-aunpzQ-t500x500.jpg',
+    src: '/music/Dreams Adrift in Coffee.mp3',
+  },
+  {
+    title: 'Whispers on a Sleepless Night',
+    artist: 'Gray Kamuix',
+    album: 'Wuthering Waves 2.8 OST',
+    cover: 'https://i1.sndcdn.com/artworks-XJKG3kwFvvs3lqBo-aunpzQ-t500x500.jpg',
+    src: '/music/Whispers on a Sleepless Night.mp3',
+  },
 ];
